4b68ebafc44a103aad78f1d7d3756835154f548c
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-05-07 18:11:28 +0530

    Time: 11 ms (92.65%), Space: 17.4 MB (83.45%) - LeetHub

--- 735-asteroid-collision/735-asteroid-collision.cpp
+++ 735-asteroid-collision/735-asteroid-collision.cpp
@@ -3,44 +3,34 @@ public:
     vector<int> asteroidCollision(vector<int>& ast) {
         
         vector<int> ans;
-        stack<int> s;
+        vector<int> s;
         int n = ast.size();
         
         for( int i = 0 ; i < n ; ++i ){
             if( ast[i] > 0 ){
-                s.push(ast[i]);
+                s.push_back(ast[i]);
             }
             else{
                 int cur = ast[i];
-                while( s.size() != 0 && cur < 0 ){
-                    int a = s.top();
-                    s.pop();
+                while( s.size() != 0&& s.back() > 0 && cur < 0 ){
+                    int a = s.back();
+                    s.pop_back();
                     if( abs(cur) == a ){
                         cur = 0;
                     }
                     else if( abs(cur) < a ){
                         cur = 0;
-                        s.push(a);
+                        s.push_back(a);
                     }
                 }
                 if( cur < 0 ){
-                    ans.push_back(cur);
+                    s.push_back(cur);
                 }
             }
         }
         
-        vector<int> c;
-        while( s.size() != 0 ){
-            c.push_back(s.top());
-            s.pop();
-        }
         
-        reverse(c.begin(),c.end());
-        for( int i = 0; i < c.size() ; ++i ){
-            ans.push_back(c[i]);
-        }
-        
-        return ans;
+        return s;
         
     }
 };
