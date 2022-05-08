4c5b9c84eff3b8d69f014decfa6822054f889d9c
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-05-08 15:42:54 +0530

    Time: 4 ms (28.34%), Space: 6.4 MB (91.04%) - LeetHub

--- 394-decode-string/394-decode-string.cpp
+++ 394-decode-string/394-decode-string.cpp
@@ -1,53 +1,39 @@
 class Solution {
-public:
-    map<int,int> m;
-        
-    string decode( string &s , int si , int ei ){
-        if( s.length() == 0 ){
-            return "";
-        }
-        
+public: 
+    string decode( string &s , int &i ){
+       
         string ans;
-        int pre = 0;
-        for( int i = si ;  i <= ei ; ++i ){
-            if( s[i] >= '0' && s[i] <= '9' ){
-                pre = pre*10 + (s[i]-'0');
+        while( i < s.length() && s[i] != ']' ){
+            
+            if( isdigit(s[i]) == 0 ){
+                ans += s[i++];
             }
-            else if( s[i] == '[' ){
-                string n = decode(s , i+1 , m[i] );
-                while( pre-- ){
-                    ans += n;
-                }
-                i = m[i];
-                pre = 0;
-            }
-            else if( s[i] == ']' ){
-                continue;
-            }   
             else{
-                ans += s[i];
+                int n = 0;
+                while( isdigit(s[i]) && i < s.length() ){
+                    n = n*10 + (s[i]-'0');
+                    ++i;
+                }
+                
+                ++i;//[
+                string smallAns = decode(s , i);
+                ++i;//]
+                
+                while( n-- ){
+                    ans += smallAns;
+                }
             }
+            
         }
         
         return ans;
+        
     }
     
     string decodeString(string s) {
-
-        stack<int> openBracket;
-
-        for( int i = 0 ; i < s.length() ; ++i ){
-            if( s[i] == '[' ){
-                openBracket.push(i);
-            }    
-            else if( s[i] == ']' ){
-                int si = openBracket.top();
-                openBracket.pop();
-                m[si] = i;
-            }
-        }
         
-        return decode(s, 0 , s.length()-1);
+        int i = 0;
+        return decode(s,i);
         
     }
 };
