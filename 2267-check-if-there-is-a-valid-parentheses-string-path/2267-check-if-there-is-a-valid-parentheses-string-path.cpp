7dba5d3410bb7d8e5611eb2915e86d2aa15af751
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-05-09 17:46:13 +0530

    Time: 638 ms (25.00%), Space: 26.2 MB (75.00%) - LeetHub

--- 2267-check-if-there-is-a-valid-parentheses-string-path/2267-check-if-there-is-a-valid-parentheses-string-path.cpp
+++ 2267-check-if-there-is-a-valid-parentheses-string-path/2267-check-if-there-is-a-valid-parentheses-string-path.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
    int mp[100][100][300];
     
-    bool isPath(vector<vector<char>> &grid , int o,int n , int m , int x , int y ){
+    bool isPath(vector<vector<char>> &grid , int o, int n , int m , int x , int y ){
 
         if( x >= n || y >= m ){
             return false;
@@ -16,25 +16,21 @@ public:
                 --o;
             }
             else{
-                return false;
+                return 0;
             }
-        }
-        
-        if( o < 0 ){
-            return false;
-        }
-        
-        if( mp[x][y][o] != -1 ){
-            return mp[x][y][o];
         }
         
         if( x == n-1 && y == m-1 ){
             return o == 0;
         }
         
+        if( mp[x][y][o] != -1 ){
+            return mp[x][y][o];
+        }
+        
         bool a = 0 , b = 0;
         
-        a = isPath(grid , o , n , m , x+1 , y );
+        a = isPath(grid , o ,  n , m , x+1 , y );
         b = isPath(grid , o , n , m , x , y+1 );
         
         return mp[x][y][o] = a||b;
@@ -47,6 +43,8 @@ public:
         if( grid[0][0] == ')' || grid[n-1][m-1] == '(' ){
             return false;
         }
+        
+        // vector<vector<int>> v;
         memset(mp,-1,sizeof(mp));
         return isPath(grid , 0 , n , m , 0 , 0 );
         
