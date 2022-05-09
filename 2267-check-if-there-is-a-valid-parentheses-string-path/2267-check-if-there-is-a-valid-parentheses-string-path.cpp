38ad6bb21d0daeb1bcffeae16db207746291e8be
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-05-09 17:34:37 +0530

    Time: 351 ms (25.00%), Space: 26.3 MB (75.00%) - LeetHub

---
+++ 2267-check-if-there-is-a-valid-parentheses-string-path/2267-check-if-there-is-a-valid-parentheses-string-path.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+   int mp[100][100][300];
+    
+    bool isPath(vector<vector<char>> &grid , int o,int n , int m , int x , int y ){
+
+        if( x >= n || y >= m ){
+            return false;
+        }
+        
+        if( grid[x][y] == '(' ){
+            ++o;
+        }
+        else{
+            if( o > 0 ){
+                --o;
+            }
+            else{
+                return false;
+            }
+        }
+        
+        if( o < 0 ){
+            return false;
+        }
+        
+        if( mp[x][y][o] != -1 ){
+            return mp[x][y][o];
+        }
+        
+        if( x == n-1 && y == m-1 ){
+            return o == 0;
+        }
+        
+        bool a = 0 , b = 0;
+        
+        a = isPath(grid , o , n , m , x+1 , y );
+        b = isPath(grid , o , n , m , x , y+1 );
+        
+        return mp[x][y][o] = a||b;
+        
+    }
+    
+    bool hasValidPath(vector<vector<char>>& grid) {
+        
+        int n = grid.size(), m = grid[0].size();
+        if( grid[0][0] == ')' || grid[n-1][m-1] == '(' ){
+            return false;
+        }
+        memset(mp,-1,sizeof(mp));
+        return isPath(grid , 0 , n , m , 0 , 0 );
+        
+    }
+};
