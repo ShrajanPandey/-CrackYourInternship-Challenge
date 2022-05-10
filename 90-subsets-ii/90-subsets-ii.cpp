aaa9b9197d668a03c78b78484bf694dde3f83b7d
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-05-10 17:48:22 +0530

    Time: 3 ms (78.89%), Space: 7.6 MB (61.66%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,24 +1,45 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    void generate(int si , vector<int> &nums , vector<int> subset){
-        ans.push_back(subset);
+//     void generate(int si , vector<int> &nums , vector<int> subset){
+//         ans.push_back(subset);
         
-        for( int i = si ; i < nums.size() ; ++i ){
-            // generate(i+1 , nums , subset);
-            if( i > si && nums[i] == nums[i-1] )continue;
-            subset.push_back(nums[i]);
-            generate(i+1 , nums , subset);
-            subset.pop_back();
-        }
+//         for( int i = si ; i < nums.size() ; ++i ){
+//             // generate(i+1 , nums , subset);
+//             if( i > si && nums[i] == nums[i-1] )continue;
+//             subset.push_back(nums[i]);
+//             generate(i+1 , nums , subset);
+//             subset.pop_back();
+//         }
 
-    }
+//     }
     
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        
         vector<int> output;
-        // vector<int> s;
-        sort(nums.begin(),nums.end());
-        generate(0 , nums , output);
+        ans.push_back(output);
+        sort(nums.begin() , nums.end());
+        
+        for( int i = 0 ; i < nums.size() ; ){
+            
+            int count = 0;//count of current element 
+            while( i+count < nums.size() && nums[i] == nums[count+i] ){
+                ++count;
+            }
+            
+            int noSubsets = ans.size();
+            for( int k = 0 ; k < noSubsets ; ++k ){
+                vector<int> newSet = ans[k];
+                for( int j = 0 ; j < count ; ++j ){
+                    newSet.push_back(nums[i]);
+                    ans.push_back(newSet);
+                }
+            }
+            
+            i += count;
+            
+        }
+        
         return ans;
     }
 };
