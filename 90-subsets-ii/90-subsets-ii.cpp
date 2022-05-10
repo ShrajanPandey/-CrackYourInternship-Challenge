09c90553152a62cea51f974cb954237926bcaef9
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-05-10 17:55:19 +0530

    Time: 11 ms (23.80%), Space: 16.6 MB (17.14%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -13,33 +13,60 @@ public:
 //         }
 
 //     }
+    //ITERATIVE 
+//     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        
+//         vector<int> output;
+//         ans.push_back(output);
+//         sort(nums.begin() , nums.end());
+        
+//         for( int i = 0 ; i < nums.size() ; ){
+            
+//             int count = 0;//count of current element 
+//             while( i+count < nums.size() && nums[i] == nums[count+i] ){
+//                 ++count;
+//             }
+            
+//             int noSubsets = ans.size();
+//             for( int k = 0 ; k < noSubsets ; ++k ){
+//                 vector<int> newSet = ans[k];
+//                 for( int j = 0 ; j < count ; ++j ){
+//                     newSet.push_back(nums[i]);
+//                     ans.push_back(newSet);
+//                 }
+//             }
+            
+//             i += count;
+            
+//         }
+        
+//         return ans;
+//     }
     
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        
-        vector<int> output;
-        ans.push_back(output);
-        sort(nums.begin() , nums.end());
-        
-        for( int i = 0 ; i < nums.size() ; ){
-            
-            int count = 0;//count of current element 
-            while( i+count < nums.size() && nums[i] == nums[count+i] ){
-                ++count;
-            }
-            
-            int noSubsets = ans.size();
-            for( int k = 0 ; k < noSubsets ; ++k ){
-                vector<int> newSet = ans[k];
-                for( int j = 0 ; j < count ; ++j ){
-                    newSet.push_back(nums[i]);
-                    ans.push_back(newSet);
-                }
-            }
-            
-            i += count;
-            
+    void subsetsWithDup(vector<int>& nums, vector<int> res, vector<vector<int>>& ans, int start) {
+        if (start == nums.size()) {
+            ans.push_back(res);
+            return;
         }
-        
+        subsetsWithDup(nums,res,ans,start+1);//do not take current 
+        // include every duplicate in te every subset and as me move ahead without it we take others 
+        // every possible subsets are counted.
+
+        while (start + 1 < nums.size() && nums[start] == nums[start+1]) {
+            res.push_back(nums[start]);
+            ++start;
+        }
+        res.push_back(nums[start]);
+
+        // include current if duplicate include every elements 
+        subsetsWithDup(nums,res,ans,start+1);
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<int> res;
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        subsetsWithDup(nums,res,ans,0);
         return ans;
     }
 };
