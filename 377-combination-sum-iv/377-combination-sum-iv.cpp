c2b4e561ab965665e50f83a6a920582eaad888b4
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-05-11 17:11:18 +0530

    Time: 5 ms (28.10%), Space: 6.5 MB (99.65%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,27 +1,33 @@
 class Solution {
-    int solve(vector<int>&nums,int target,vector<int>&dp){
+public:
+    int dp[2000];
+    
+    int solve( vector<int>&nums , int target ){
         
-        if(target==0) return 1;
-        if(dp[target]!=-1) return dp[target];
-        
-        int cnt=0;
-        for(int i=0;i<nums.size();i++){
-            
-            if(target>=nums[i])cnt+=solve(nums,target-nums[i],dp);
-            
+        if( target == 0 ){
+            return 1;
+        }
+        if( target < 0 ){
+            return 0;
         }
         
+        if( dp[target] != -1 ){
+            return dp[target];
+        }
+        int ans = 0;
+        for( int i = 0 ; i < nums.size() ; ++i ){
+            if( target >= nums[i] )
+            ans += solve(nums , target-nums[i] );
+        }
         
-        return dp[target]=cnt;
+        return dp[target] = ans;
         
     }
     
-public:
-    int combinationSum4(vector<int>& nums, int target) {
-     
+    int combinationSum4(vector<int>& nums, int target ) {
         
-        vector<int>dp(target+1,-1);
-        return solve(nums,target,dp);
+        memset(dp,-1,sizeof(dp));
+        return solve(nums , target);
         
     }
 };
