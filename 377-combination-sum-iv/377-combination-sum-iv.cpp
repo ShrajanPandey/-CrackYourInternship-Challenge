dc084d98776b426cb2cd89032011cfd2b6f69553
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-05-11 17:13:30 +0530

    Time: 4 ms (39.99%), Space: 6.7 MB (67.96%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,33 +1,47 @@
 class Solution {
 public:
-    int dp[2000];
+    // int dp[2000];
     
-    int solve( vector<int>&nums , int target ){
+//     int solve( vector<int>&nums , int target ){
         
-        if( target == 0 ){
-            return 1;
-        }
-        if( target < 0 ){
-            return 0;
-        }
+//         if( target == 0 ){
+//             return 1;
+//         }
+//         if( target < 0 ){
+//             return 0;
+//         }
         
-        if( dp[target] != -1 ){
-            return dp[target];
-        }
-        int ans = 0;
-        for( int i = 0 ; i < nums.size() ; ++i ){
-            if( target >= nums[i] )
-            ans += solve(nums , target-nums[i] );
-        }
+//         if( dp[target] != -1 ){
+//             return dp[target];
+//         }
+//         int ans = 0;
+//         for( int i = 0 ; i < nums.size() ; ++i ){
+//             if( target >= nums[i] )
+//             ans += combinationSum4(nums , target-nums[i] );
+//         }
         
-        return dp[target] = ans;
+//         return dp[target] = ans;
         
-    }
+//     }
     
     int combinationSum4(vector<int>& nums, int target ) {
         
-        memset(dp,-1,sizeof(dp));
-        return solve(nums , target);
+        unsigned int dp[target+1];
+        memset(dp,0,sizeof(dp));
+        dp[0] = 1;
+        
+        sort(nums.begin(),nums.end());
+        
+        for( int i = 1 ; i <= target ; ++i ){
+            for( int j = 0 ; j < nums.size() ; ++j ){
+                if( nums[j] > i ){
+                    break;
+                }
+                dp[i] += dp[i-nums[j]];
+            }
+        }
+        
+        return dp[target];
         
     }
 };
