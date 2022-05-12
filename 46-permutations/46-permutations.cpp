962ba808212fbee291a9ce811e223649a03d2c43
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-05-12 23:29:14 +0530

    Time: 9 ms (18.34%), Space: 8 MB (38.00%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -3,29 +3,45 @@ public:
     vector<vector<int>> ans;
     vector<int> p;
     
+    void solve(vector<int> &nums , int begin ){
+        
+        if( begin >= nums.size() ){
+            ans.push_back(nums);
+            return;
+        }
+        
+        for(int i = begin ; i < nums.size() ; ++i ){
+            swap(nums[begin],nums[i]);
+            solve(nums , begin+1 );
+            swap(nums[begin],nums[i]);
+        }
+        
+    }
+    
     vector<vector<int>> permute(vector<int>& nums) {
         
-        if( p.size() == nums.size() ){
-            ans.push_back(p);
-            return ans;
-        }
+//         if( p.size() == nums.size() ){
+//             ans.push_back(p);
+//             return ans;
+//         }
         
-        for( int i = 0 ; i < nums.size() ; ++i ){
+//         for( int i = 0 ; i < nums.size() ; ++i ){
             
-            bool isp = 0;
-            for( int j = 0 ; j < p.size() ; ++j ){
-                if( p[j] == nums[i] ){
-                    isp = 1;
-                    break;
-                }
-            }
+//             bool isp = 0;
+//             for( int j = 0 ; j < p.size() ; ++j ){
+//                 if( p[j] == nums[i] ){
+//                     isp = 1;
+//                     break;
+//                 }
+//             }
             
-            if( isp == 0 ){
-                p.push_back(nums[i]);
-                permute(nums);
-                p.pop_back();
-            }
-        }
+//             if( isp == 0 ){
+//                 p.push_back(nums[i]);
+//                 permute(nums);
+//                 p.pop_back();
+//             }
+//         }
+        solve( nums , 0);
         
         return ans;
         
