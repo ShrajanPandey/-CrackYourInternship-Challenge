f0be64b45fb80539188716b296910ae89e2e7d94
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-05-15 13:10:55 +0530

    Time: 990 ms (12.64%), Space: 8 MB (74.89%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -2,35 +2,38 @@ class Solution {
 public:
     int n , m;
     
-    bool isExist( vector<vector<char>> &board , string word , vector<vector<int>> &v , int x , int y ){
+    bool isExist( vector<vector<char>> &board , string word , int x , int y ){
     
         if( word.length() == 0 ){
             return true;
         }
         
-        if( x == n || y == m || word[0] != board[x][y] ){
+        if( x < 0 || y < 0 || x == n || y == m || word[0] != board[x][y] ){
             return false;
         }
         if( word.length() == 1 && board[x][y] == word[0] ){
             return true;
         }
         
-        v[x][y] = 1;
+        // v[x][y] = 1;
+        char a = board[x][y];
+        board[x][y] = '-';
         bool ans = 0;
-        if( x < n-1 &&  v[x+1][y] == 0 && isExist(board , word.substr(1) , v , x+1 , y)){
+        if( x < n-1 && isExist(board , word.substr(1)  , x+1 , y)){
             return true;
         }
-        if( y < m-1 && v[x][y+1] == 0 && isExist(board , word.substr(1), v,  x , y+1) ){
+        if( y < m-1 && isExist(board , word.substr(1),  x , y+1) ){
             return true;
         }
-        if( x > 0 && v[x-1][y] == 0 && isExist(board , word.substr(1) , v ,  x-1 , y) ){
+        if( x > 0 && isExist(board , word.substr(1)  ,  x-1 , y) ){
             return true;
         }
-        if( y > 0 && v[x][y-1] == 0 && isExist(board , word.substr(1) , v , x, y-1) ){
+        if( y > 0 && isExist(board , word.substr(1) , x, y-1) ){
             return true;
         }
         
-        v[x][y] = 0;
+        // v[x][y] = 0;
+        board[x][y] = a;
         return false;
         
     }
@@ -46,7 +49,7 @@ public:
                 if( word.length() == 1 && board[i][j] == word[0] ){
                     return true;
                 }
-                if( board[i][j] == word[0] && isExist(board , word , visit , i , j ) ){
+                if( board[i][j] == word[0] && isExist(board , word , i , j ) ){
                     return true;
                 }
             }
