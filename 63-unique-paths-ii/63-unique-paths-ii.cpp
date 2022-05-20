2417d637e2970c22a7710f7c038ca386f24f9731
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-05-20 13:31:10 +0530

    Time: 6 ms (36.45%), Space: 7.8 MB (60.56%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,33 +1,47 @@
 class Solution {
 public:
     
-    map<pair<int,int>,int> visited;
-    int uniquePaths(vector<vector<int>> &hurdle , int n , int m ){
+//     map<pair<int,int>,int> visited;
+//     int uniquePaths(vector<vector<int>> &hurdle , int n , int m ){
         
-        if( n == 1 && m == 1 ){
-            return 1;    
-        }
-        if( n == 0 || m == 0 || hurdle[n-1][m-1] == 1 ){
-            return 0;
-        }
+//         if( n == 1 && m == 1 ){
+//             return 1;    
+//         }
+//         if( n == 0 || m == 0 || hurdle[n-1][m-1] == 1 ){
+//             return 0;
+//         }
         
-        if( visited.count(make_pair(n,m)) ){
-            return visited[make_pair(n,m)];
-        }
+//         if( visited.count(make_pair(n,m)) ){
+//             return visited[make_pair(n,m)];
+//         }
         
-        int ans = uniquePaths(hurdle , n-1 , m ) + uniquePaths(hurdle , n , m-1 );
-        return visited[make_pair(n,m)] = ans;
+//         int ans = uniquePaths(hurdle , n-1 , m ) + uniquePaths(hurdle , n , m-1 );
+//         return visited[make_pair(n,m)] = ans;
         
-    }
+//     }
     
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         
-        int n = obstacleGrid.size(),m = obstacleGrid[0].size();
-        if( obstacleGrid[0][0] == 1 || obstacleGrid[n-1][m-1] == 1 ){
-            return 0;
+        int n = obstacleGrid.size() , m = obstacleGrid[0].size();
+        vector<int> dp(m,0);
+        dp[0] = 1;
+        
+        for( vector<int> row : obstacleGrid ){
+            
+            for( int i = 0 ; i < m ; ++i ){
+                
+                if( row[i] == 1 ){
+                    dp[i] = 0;
+                }
+                else if( i > 0 ){
+                    dp[i] = dp[i] + dp[i-1];
+                }
+                
+            }
+            
         }
         
-        return uniquePaths(obstacleGrid, n , m );
+        return dp[m-1];
         
     }
 };
