b5501d26e9624c08c714ef8ab0a2c8f7e1643cb2
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-05-21 12:23:05 +0530

    Time: 51 ms (95.24%), Space: 14.2 MB (73.43%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,48 +1,42 @@
 class Solution {
 public:
-    int m[100000];
-    int ans(vector<int> coins ,int amount ){
+    int coinChange(vector<int>& coins, int amount) {
+        
         if( amount == 0 ){
             return 0;
         }
-        if( amount < 0 ){
-            return INT_MAX;
-        }
         
-        if( m[amount] != -1 ){
-            return m[amount];
-        }
+        queue<int> que;
+        que.push(amount);
+        vector<bool> visited( amount , 0);
+        int curLevel = 1;
         
-        int ans1 = INT_MAX;
-        for( int i = 0 ; i < coins.size() ; ++i ){
-            if( amount >= coins[i] ){
-                int p = ans(coins , amount-coins[i]);
-                ans1 = min(ans1 ,(p == INT_MAX ? p : p+1));
-            }
-        }
-        // cout << amount << " " << ans1 << endl;
-        return m[amount] = ans1;
-    }
-    
-    int coinChange(vector<int>& coins, int amount) {
-        
-//         memset(m , -1 , sizeof(m));
-//         int a = ans( coins , amount );
-        
-//         return ( a == INT_MAX ? -1 : a );
-        vector<int> dp(amount+1);
-        dp[0] = 0;
-        
-        for( int i = 1; i < amount+1 ; ++i ){
-            int ans = INT_MAX;
-            for( int j = 0 ; j < coins.size() ; ++j ){
-                if( coins[j] <= i ){
-                    ans = min(ans , (dp[i-coins[j]] == INT_MAX ? dp[i-coins[j]] : dp[i-coins[j]]+1));
+        while( que.size() != 0 ){
+            
+            int size = que.size();
+            for( int i = 0 ; i < size ; ++i ){
+                
+                int cur = que.front();
+                que.pop();
+                
+                for( auto r : coins ){
+                    int curval = cur - r;
+                    if( curval == 0 ){
+                        return curLevel;
+                    }
+                    else if( curval > 0 && visited[curval] == 0 ) {
+                        visited[curval] = 1;
+                        que.push(curval);
+                    }
                 }
+                
             }
-            dp[i] = ans;
+            
+            ++curLevel;
+            
         }
-                              
-        return dp[amount] == INT_MAX ? -1 : dp[amount];
+        
+        return -1;
+        
     }
 };
