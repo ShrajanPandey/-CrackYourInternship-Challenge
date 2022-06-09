472aa64a01d66b26aba5a858e0fcbb627ea2efd8
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-06-09 23:33:29 +0530

    Time: 3 ms (86.09%), Space: 11 MB (95.77%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -2,38 +2,66 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         
-        int n = nums.size();
-        int left = 0 , right = nums.size()-1;
-        while( left < right ){
-            int mid = left + ( right - left )/2;
-            if( nums[mid] < nums[right] ){
-                right = mid;
-            }
-            else{
-                left = mid+1;
-            }   
-        }
-        //we found smalles element hence its index equals the degree of rotation 
-        // we assume array to be a larger array and hence calcaulate index as per %n
-        int rot = left;
-        left = 0 , right = nums.size()-1;
+        int n = nums.size(), left = 0 , right = n-1;
         
         while( left <= right ){
             
             int mid = left + ( right - left )/2;
-            int realMid = (mid + rot)%n;
             
-            if( nums[realMid] == target ){
-                return realMid;
+            if( nums[mid] == target ){
+                return mid;
             }
-            else if( nums[realMid] > target ){
-                right = mid-1;
+            
+            if( nums[left] <= nums[mid] ){
+                if( nums[left] <= target && nums[mid] > target ){
+                    right = mid-1;
+                }
+                else{
+                    left= mid+1;
+                }
             }
             else{
-                left = mid+1;
+                if( nums[right] >= target && nums[mid] < target ){
+                    left = mid+1;
+                }
+                else{
+                    right = mid-1;
+                }
             }
-            
         }
+        
+//         int n = nums.size();
+//         int left = 0 , right = nums.size()-1;
+//         while( left < right ){
+//             int mid = left + ( right - left )/2;
+//             if( nums[mid] < nums[right] ){
+//                 right = mid;
+//             }
+//             else{
+//                 left = mid+1;
+//             }   
+//         }
+//         //we found smalles element hence its index equals the degree of rotation 
+//         // we assume array to be a larger array and hence calcaulate index as per %n
+//         int rot = left;
+//         left = 0 , right = nums.size()-1;
+        
+//         while( left <= right ){
+            
+//             int mid = left + ( right - left )/2;
+//             int realMid = (mid + rot)%n;
+            
+//             if( nums[realMid] == target ){
+//                 return realMid;
+//             }
+//             else if( nums[realMid] > target ){
+//                 right = mid-1;
+//             }
+//             else{
+//                 left = mid+1;
+//             }
+            
+//         }
         
         return -1;
         
