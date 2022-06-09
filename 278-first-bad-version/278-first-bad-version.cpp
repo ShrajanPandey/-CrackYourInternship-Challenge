db48cd55c7f25e5afcb94d76607a65ee7c32f264
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-06-09 23:56:31 +0530

    Time: 5 ms (9.48%), Space: 5.9 MB (23.28%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -10,9 +10,9 @@ public:
         while( left < right ){
             
             int mid = left + ( right - left )/2;
-            if( mid < n && isBadVersion(mid) == 0 && isBadVersion(mid+1) ){
-                return mid+1;
-            }
+            // if( mid < n && isBadVersion(mid) == 0 && isBadVersion(mid+1) ){
+            //     return mid+1;
+            // }
             
             if( isBadVersion(mid) == 0 ){
                 left = mid+1;
