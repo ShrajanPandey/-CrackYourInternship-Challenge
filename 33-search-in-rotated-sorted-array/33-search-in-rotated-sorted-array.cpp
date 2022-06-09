02345854bb0aa7f70d5fec16f38c470f46e8377b
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-06-09 23:22:30 +0530

    Time: 3 ms (86.09%), Space: 11.1 MB (29.96%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         
-       int left = 0 , right = nums.size()-1;
+        int n = nums.size();
+        int left = 0 , right = nums.size()-1;
         while( left < right ){
             int mid = left + ( right - left )/2;
             if( nums[mid] < nums[right] ){
@@ -10,36 +11,58 @@ public:
             }
             else{
                 left = mid+1;
-            }
-            
+            }   
         }
-        
-        int smallIdx = left;
-        if( nums[smallIdx] == target ){
-            return smallIdx;
-        }
+        //we found smalles element hence its index equals the degree of rotation 
+        // we assume array to be a larger array and hence calcaulate index as per %n
+        int rot = left;
         left = 0 , right = nums.size()-1;
-        if( target > nums[smallIdx] && nums[right] < target ){
-            right = smallIdx;
-        }
-        else{
-            left = smallIdx;
-        }
         
         while( left <= right ){
+            
             int mid = left + ( right - left )/2;
-            if( nums[mid] == target ){
-                return mid;
+            int realMid = (mid + rot)%n;
+            
+            if( nums[realMid] == target ){
+                return realMid;
             }
-            else if( nums[mid] > target ){
+            else if( nums[realMid] > target ){
                 right = mid-1;
             }
             else{
                 left = mid+1;
             }
+            
         }
         
         return -1;
+        
+//         int smallIdx = left;
+//         if( nums[smallIdx] == target ){
+//             return smallIdx;
+//         }
+//         left = 0 , right = nums.size()-1;
+//         if( target > nums[smallIdx] && nums[right] < target ){
+//             right = smallIdx;
+//         }
+//         else{
+//             left = smallIdx;
+//         }
+        
+//         while( left <= right ){
+//             int mid = left + ( right - left )/2;
+//             if( nums[mid] == target ){
+//                 return mid;
+//             }
+//             else if( nums[mid] > target ){
+//                 right = mid-1;
+//             }
+//             else{
+//                 left = mid+1;
+//             }
+//         }
+        
+//         return -1;
         
     }
 };
