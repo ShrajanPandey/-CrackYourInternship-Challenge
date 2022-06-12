6a5643d00ef2353edb72881c0b2ca5cc96341dbb
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-06-12 22:59:58 +0530

    Time: 1017 ms (5.11%), Space: 156.3 MB (8.99%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -1,36 +1,60 @@
 class Solution {
 public:
     //used prefix sum for storng all the sum before any calculations 
-    //used unordered map to 
+    //used unordered map to store last occurance index of an element
+    //whenever any elements repeats itself just calculate the sum and update the index of that element 
     int maximumUniqueSubarray(vector<int>& nums) {
         
-        int s = 0;
-        map<int,int> m;
-        int ansS = 0;
+        int i = 0 , j = 0;
+        int ans = 0 , sum = 0;
+        set<int> s;
         
-        vector<int> p;
-        for( int i = 0 ; i < nums.size() ;++i ){
-            s += nums[i];
-            p.push_back(s);
-        }
-        
-        s = 0;
-        for( int i = 0 ; i < nums.size() ; ++i ){
-            if( m.count(nums[i]) && m[nums[i]] >= s ){
-                
-                ansS = max( ansS , p[i-1]-p[s] + nums[s]);
-                s = max(s , m[nums[i]]+1);
-                m[nums[i]] = i;
-                
+        while( i < nums.size() && j < nums.size() ){
+            
+            if( s.count(nums[j]) == 0 ){
+                sum += nums[j];
+                ans = max( ans , sum);
+                s.insert(nums[j]);
+                ++j;
             }
             else{
-                m[nums[i]] = i;
+                sum -= nums[i];
+                s.erase(nums[i]);
+                ++i;
             }
-        }
-
-        ansS = max( ansS , p[nums.size()-1] - p[s] + nums[s] );
+            
+        }  
         
-        return ansS;
+        return ans;
+        
+        
+//         int s = 0;
+//         unordered_map<int,int> m;
+//         int ansS = 0;
+        
+//         vector<int> p;
+//         for( int i = 0 ; i < nums.size() ;++i ){
+//             s += nums[i];
+//             p.push_back(s);
+//         }
+        
+//         s = 0;
+//         for( int i = 0 ; i < nums.size() ; ++i ){
+            
+//             if( m.count(nums[i]) && m[nums[i]] >= s ){
+                
+//                 ansS = max( ansS , p[i-1]-p[s] + nums[s]);
+//                 s = max(s , m[nums[i]]+1);
+        
+//             }
+            
+//             m[nums[i]] = i;
+            
+//         }
+
+//         ansS = max( ansS , p[nums.size()-1] - p[s] + nums[s] );
+        
+//         return ansS;
         
     }
 };
