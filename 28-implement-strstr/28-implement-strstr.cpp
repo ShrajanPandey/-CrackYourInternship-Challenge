114ea703f450a7531803e3a5e033ae7472da4d24
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-06-20 22:36:11 +0530

    Time: 3 ms (43.68%), Space: 6.3 MB (59.78%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        
+        if( needle.length() == 0 ){
+            return 0;
+        }
+        
+        if( needle.length() > haystack.length() ){
+            return -1;
+        }
+
+        for( int i = 0 ; i < haystack.length() ; ++i ){
+            
+            if( haystack[i] == needle[0] ){
+                
+                int j = 0, k = i;
+                bool ans = 1;
+                
+                while( j < needle.length() && k < haystack.length() ){
+                    if( needle[j] != haystack[k] ){
+                        ans = 0;
+                        break;
+                    }
+                    ++j;
+                    ++k;
+                }
+                
+                if( ans && j == needle.length() ){
+                    return i;
+                }
+                
+            }
+            
+        }
+        
+        return -1;
+        
+    }
+};
