083e867db0a9a15e4365b721bfdd4ef003ddeca7
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-06-22 23:46:56 +0530

    Time: 3 ms (76.80%), Space: 8.8 MB (22.16%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
@@ -17,30 +17,23 @@ public:
         }
         
         int lastDigit = curNo[curNo.length()-1]-'0';
-        int one = abs(lastDigit-k);
-        int two = abs(lastDigit+k);
+        int one = lastDigit+k;
         
-        if( one != two ){
-            if( one < 10 && abs(one-lastDigit) == k )
-                generateAll(n-1 , k , curNo + (char)(one + '0') , ans);
-            if( two < 10 && abs(two-lastDigit) == k ){
-                generateAll(n-1 , k , curNo + (char)(two + '0') , ans);
-            }
-        }
-        else{
-            
-            if( abs(one-lastDigit) == k && one < 10 ){
-                generateAll(n-1 , k , curNo + (char)(one + '0') , ans );
-            }
+        if( one < 10 && abs(one-lastDigit) == k ){
+            generateAll(n-1 , k , curNo + (char)(one + '0') , ans);
         }
         
+        int two = lastDigit-k;
+        if( k != 0 && two >= 0 ){
+            generateAll(n-1 , k , curNo + (char)(two + '0') , ans);
+        }
+     
     }
     
     vector<int> numsSameConsecDiff(int n, int k) {
         
         vector<int> ans;
         generateAll(n , k , "" , ans);
-        
         return ans;
         
     }
