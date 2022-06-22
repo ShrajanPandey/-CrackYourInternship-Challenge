281ec73f6fc2a6ddb20fa47d033697ed42360689
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-06-22 16:23:47 +0530

    Time: 23 ms (13.12%), Space: 17.4 MB (20.79%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -1,52 +1,42 @@
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        
+       
         if( root == NULL ){
             return 0;
         }
         
-        unsigned int ans = 0;
-        queue<TreeNode *> pend;
-        map<TreeNode *,unsigned int> m;
-        m[root] = 1;
-        pend.push(root);
+        int ans = 0;
+        queue<pair<TreeNode*,long long>> pending;
+        pending.push({root,1});
         
-        unsigned int cur = 0 , maxWidth = 0;
-        
-        while( pend.size() != 0 ){
+        while( pending.empty() != 1 ){
             
-            int size = pend.size();
-            unsigned int start = -1, end = -1;
+            long long start = pending.front().second;
+            long long end = pending.back().second;
             
+            int size = pending.size();
             for( int i = 0 ; i < size ; ++i ){
                 
-                TreeNode* curNode = pend.front();
-                pend.pop();
+                pair<TreeNode*,int> curPair = pending.front();
+                pending.pop();
                 
-                if( start == -1 ){
-                    start = m[curNode];
-                    end = start;
-                }
-                unsigned int idx = m[curNode];
-                end = idx;
+                TreeNode *curNode = curPair.first;
                 
                 if( curNode->left ){
-                    pend.push(curNode->left);
-                    m[curNode->left] = 2*idx;
+                    pending.push({curNode->left,(long long)2*curPair.second + 1});
                 }
                 if( curNode->right ){
-                    pend.push(curNode->right);
-                    m[curNode->right] = 2*idx + 1;
+                    pending.push({curNode->right, (long long)2*curPair.second + 2});
                 }
+                
+                ans = max((long long) ans , end - start+1 );
                 
             }
             
-            maxWidth = max( maxWidth , end-start+1 );
-            
         }
         
-        return maxWidth;
+        return ans;
         
     }
 };
