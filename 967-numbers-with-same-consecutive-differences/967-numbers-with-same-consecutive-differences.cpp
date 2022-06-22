b95fc358ac3a9d1c337bdd305d433531d9ce2bd1
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-06-23 00:01:48 +0530

    Time: 4 ms (59.28%), Space: 7.1 MB (93.56%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
@@ -19,11 +19,43 @@ public:
     
     vector<int> numsSameConsecDiff(int n, int k) {
         
-        vector<int> ans;
-                 
-        string curNo;
+        queue<int> numbers;
         for( int i = 1 ; i <= 9 ; ++i ){
-            generateAll(n-1 , k , curNo + (char)(i+'0'), ans );
+            numbers.push(i);
+        }
+        
+        --n;
+        while( n-- ){
+            
+            int size = numbers.size();
+        
+            for( int i = 0 ; i < size ; ++i ){
+                
+                int curNum = numbers.front();
+                int lastDigit = curNum%10;
+                
+                numbers.pop();
+                
+                if(lastDigit + k <= 9 ){
+                    numbers.push(curNum*10 + lastDigit+k);
+                }
+                if( k != 0 && lastDigit-k >= 0 ){
+                    numbers.push(curNum*10 + lastDigit-k);
+                }
+                
+            }
+            
+            if( n == 0 ){
+                break;
+            }
+            
+        }
+        
+        vector<int> ans;
+        
+        while( numbers.empty() != 1 ){
+            ans.push_back(numbers.front());
+            numbers.pop();
         }
         
         return ans;
