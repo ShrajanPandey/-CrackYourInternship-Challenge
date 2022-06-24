cdc402a62b80be8d198a7b96b7260b791a8b1d1d
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-06-24 17:00:18 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (21.47%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
@@ -12,21 +12,17 @@ class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
      
-        if( head == NULL ){
+        if( head == NULL || head->next == NULL ){
             return head;
         }
         
-        ListNode *slow = head , *fast = head->next;
+        ListNode *slow = head , *fast = head;
         
         while( fast && fast->next ){
             
             fast = fast->next->next;
             slow = slow->next;
             
-        }
-        
-        if( fast != NULL ){
-            slow = slow->next;
         }
         
         return slow;
