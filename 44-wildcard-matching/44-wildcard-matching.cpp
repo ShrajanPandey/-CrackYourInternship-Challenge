64d5460a020547152ecfe50131f64aff262b9a77
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-06-28 02:08:44 +0530

    Time: 1006 ms (5.00%), Space: 28.8 MB (5.04%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
 
-    bool isMatching( string &s , string &p , int curS , int curP , vector<vector<int>> &arr){
+    int q = 0;
+    bool isMatching( string &s , string &p , int curS , int curP , map<int,int> &after , vector<vector<int>> &arr ){
         
         // cout << curS << " M " << curP << endl;
         
@@ -28,17 +29,18 @@ public:
         }
         
         if( s[curS] == p[curP] || p[curP] == '?' ){
-            return arr[curS][curP] = isMatching( s, p , curS+1 , curP+1 , arr );
+            return arr[curS][curP] = isMatching( s, p , curS+1 , curP+1 , after , arr);
         }
         
         if( p[curP] == '*' ){
             
-            for( int j = curS ; j <= s.length() ; ++j ){
-                
-                if( isMatching( s , p , j , curP+1 , arr ) ){
+            int possibleMoves = s.length() - after[curP] - curP;
+            cout << possibleMoves << endl;
+            for( int i = 0 ; i <= possibleMoves ; ++i ){
+
+                if( isMatching( s, p , curS+i , curP+1 , after , arr) ){
                     return arr[curS][curP] = 1;
                 }
-                
             }
             
         }
@@ -49,9 +51,34 @@ public:
     
     bool isMatch(string s, string p) {
         
-        vector<vector<int>> arr(s.length()+1 , vector<int> (p.length()+1 , -1));
+        int q = 0;
         
-        return isMatching( s , p , 0 , 0 , arr);
+        map<int,int> noAfter;
+        int c = 0;
+        
+        for( int i = p.length()-1 ; i >= 0 ;--i ){
+            
+            if( p[i] == '*' ){
+                noAfter[i] = c;
+            }
+            else 
+                ++c;
+            
+        }
+        
+        for( int j = 0 ; j < p.length() ; ++j ){
+            
+            if( p[j] == '*' ){
+                noAfter[j] -= q;
+                ++q;
+            }
+            
+        }
+        
+        // memset(arr , -1 , sizeof(arr));
+        vector<vector<int>> arr(s.length()+1 , vector<int> (p.length()+1,-1));
+        
+        return isMatching( s , p , 0 , 0, noAfter , arr );
         
     }
 };
