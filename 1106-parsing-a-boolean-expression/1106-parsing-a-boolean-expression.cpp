c5887925ab0bfa3b7428d592be301783d9ac0b79
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-06-28 01:01:11 +0530

    Time: 48 ms (13.13%), Space: 9.2 MB (21.56%) - LeetHub

---
+++ 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    bool parseBoolExpr(string expression) {
+        
+        stack<char> toEval;
+
+        for( int i = 0 ; i < expression.length() ; ++i ){
+            
+            if( expression[i] == ')' ){
+                
+                vector<char> smallExpressions;
+                
+                while( toEval.empty() != 1 ){
+                    
+                    char c = toEval.top();
+                    toEval.pop();
+                    
+                    if( c == '(' ){
+                        break;
+                    }
+                    else if( c == ',' ){
+                        continue;
+                    } 
+                    else{
+                        smallExpressions.push_back(c);
+                    }
+                    
+                }
+    
+                bool ans = smallExpressions[0] == 't';
+                if( toEval.empty() ){
+                    toEval.push(( ans == 1 ? 't' : 'f'));
+                    break;
+                }
+                
+                char operation = toEval.top();
+                toEval.pop();
+                
+                if( operation == '&' ){
+                    for( int i = 0 ; i < smallExpressions.size() ; ++i ){
+                        ans &= ( smallExpressions[i] == 't' );
+                    }
+                }
+                else if( operation == '|' ){
+                    for( int i = 0 ; i < smallExpressions.size() ; ++i ){
+                        ans |= ( smallExpressions[i] == 't' );
+                    }
+                }
+                else if( operation == '!' ){
+                    ans ^= 1;
+                }
+                toEval.push(( ans == 1 ? 't' : 'f') );
+
+            }
+            else{
+                toEval.push(expression[i]);
+            }
+            
+        }
+        
+        return toEval.top() == 't';
+        
+    }
+};
