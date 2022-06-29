791b989320ea274bb078adc662eb1d2f51876ccb
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-06-30 00:33:40 +0530

    Time: 11 ms (59.02%), Space: 13.3 MB (25.62%) - LeetHub

---
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -0,0 +1,77 @@
+class Solution {
+public:
+    int bfs( vector<vector<int>> &grid ){
+        
+        vector<vector<int>> directions = {{1,0},{-1,0},{0,1},{0,-1}};
+        
+        queue<pair<int,int>> rottenOranges;
+        int n = grid.size() , m = grid[0].size();
+        
+        for( int i = 0 ;i < n ; ++i ){
+
+            for( int j = 0 ;j < m ; ++j ){
+                if( grid[i][j] == 2 ){
+                    rottenOranges.push({i,j});
+                }
+            }
+            
+        }
+        
+        int time = 0;
+        
+        while( rottenOranges.empty() != 1 ){
+            
+            int noOranges = rottenOranges.size();
+            
+            for( int i = 0 ; i < noOranges ; ++i ){
+                
+                pair<int,int> curOrange = rottenOranges.front();
+                rottenOranges.pop();
+                
+                int x = curOrange.first;
+                int y = curOrange.second;
+                
+                for( auto &cur : directions ){
+                    
+                    int x1 = x + cur[0];
+                    int y1 = y + cur[1];
+                    
+                    if( x1 < 0 || y1 < 0 || x1 >= n || y1 >= m ){
+                        continue;
+                    }
+                    
+                    if( grid[x1][y1] == 1 ){
+                        grid[x1][y1] = 2;
+                        rottenOranges.push({x1,y1});    
+                    }
+                    
+                }
+                
+            }
+            
+            if( rottenOranges.size() )
+                ++time;
+            
+        }
+        
+        
+        return time;
+        
+    }
+    
+    int orangesRotting(vector<vector<int>>& grid) {
+        
+        int minTime = bfs(grid);
+        
+        for( int i = 0 ; i < grid.size() ; ++i ){
+            for( int j = 0 ; j < grid[i].size() ; ++j ){
+                if( grid[i][j] == 1 ){
+                    return -1;
+                }
+            }
+        }
+        
+        return minTime;
+        
+    }
+};
