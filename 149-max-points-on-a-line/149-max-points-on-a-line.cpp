dec61d1893a8885bc0878ba6e93bf1a9cbad63eb
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-06-30 17:10:50 +0530

    Time: 83 ms (42.97%), Space: 22 MB (14.33%) - LeetHub

---
+++ 149-max-points-on-a-line/149-max-points-on-a-line.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    long double slope( vector<int> &a , vector<int> &b ){
+        
+        long double x = a[0] , y = a[1] ,x1 = b[0] , y1 = b[1];
+        long double s = (y1-y)/(x1-x);
+        return s;
+        
+    }
+    
+    int maxPoints(vector<vector<int>>& points) {
+        
+        int fans = 1;
+        
+        for( int i = 0 ; i < points.size() ; ++i ){
+            
+            int ans = 0;
+            map<long double,int> slopes;
+            for( int j = 0 ; j < points.size() ; ++j ){
+                
+                if( i == j ){
+                    continue;
+                }
+                
+                long double curSlope = slope(points[i],points[j]);
+                slopes[curSlope]++;
+                // cout << curSlope << ' ' << i << ' ' << j << endl;
+                // slopes[curSlope].insert(points[i]);
+                // slopes[curSlope].insert(points[j]);
+                ans = max( ans , (int)slopes[curSlope]+1 );
+                
+            }
+            
+            fans = max( fans , ans);
+        
+        }
+        
+        return fans;
+        
+    }
+};
