cada2729a2b6511b4a06ff677037898197aff1c0
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-06-30 18:30:40 +0530

    Time: 294 ms (84.80%), Space: 8 MB (73.37%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int n , m;
     
-    bool isExist( vector<vector<char>> &board , string &word , int cur , vector<vector<int>> &v , int x , int y ){
+    bool isExist( vector<vector<char>> &board , string &word , int cur , int x , int y ){
     
         if( cur >= word.length() ){
             return true;
@@ -15,22 +15,25 @@ public:
             return true;
         }
         
-        v[x][y] = 1;
+        
         bool ans = 0;
-        if( x < n-1 &&  v[x+1][y] == 0 && isExist(board , word , cur+1 , v , x+1 , y)){
+        char c = board[x][y];
+        board[x][y] ='-';
+        
+        if( x < n-1 &&  board[x+1][y] != '-' && isExist(board , word , cur+1 , x+1 , y)){
             return true;
         }
-        if( y < m-1 && v[x][y+1] == 0 && isExist(board , word , cur+1 , v,  x , y+1) ){
+        if( y < m-1 && board[x][y+1] != '-' && isExist(board , word , cur+1 ,  x , y+1) ){
             return true;
         }
-        if( x > 0 && v[x-1][y] == 0 && isExist(board , word , cur+1 , v ,  x-1 , y) ){
+        if( x > 0 && board[x-1][y] != '-' && isExist(board , word , cur+1 ,  x-1 , y) ){
             return true;
         }
-        if( y > 0 && v[x][y-1] == 0 && isExist(board , word , cur+1 , v , x, y-1) ){
+        if( y > 0 && board[x][y-1] != '-' && isExist(board , word , cur+1 , x, y-1) ){
             return true;
         }
         
-        v[x][y] = 0;
+        board[x][y] = c;
         return false;
         
     }
@@ -38,7 +41,7 @@ public:
     bool exist(vector<vector<char>>& board, string word) {
         
         n = board.size() , m = board[0].size();
-        vector<vector<int>> visit(n , vector<int> (m,0));
+        // vector<vector<int>> visit(n , vector<int> (m,0));
         
         for( int i = 0 ; i < n ; ++i ){
             for( int j = 0 ; j < m ; ++j ){
@@ -46,7 +49,7 @@ public:
                 if( word.length() == 1 && board[i][j] == word[0] ){
                     return true;
                 }
-                if( board[i][j] == word[0] && isExist(board , word , 0 , visit , i , j ) ){
+                if( board[i][j] == word[0] && isExist(board , word , 0 , i , j ) ){
                     return true;
                 }
             }
