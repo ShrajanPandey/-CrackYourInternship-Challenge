4cf707622655ec62538b87d4ef42f37b0467b26b
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-06-30 18:27:12 +0530

    Time: 344 ms (76.05%), Space: 8.1 MB (35.45%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -2,31 +2,31 @@ class Solution {
 public:
     int n , m;
     
-    bool isExist( vector<vector<char>> &board , string word , vector<vector<int>> &v , int x , int y ){
+    bool isExist( vector<vector<char>> &board , string &word , int cur , vector<vector<int>> &v , int x , int y ){
     
-        if( word.length() == 0 ){
+        if( cur >= word.length() ){
             return true;
         }
         
-        if( x == n || y == m || word[0] != board[x][y] ){
+        if( x == n || y == m || word[cur] != board[x][y] ){
             return false;
         }
-        if( word.length() == 1 && board[x][y] == word[0] ){
+        if( cur == word.length()-1 && board[x][y] == word[cur] ){
             return true;
         }
         
         v[x][y] = 1;
         bool ans = 0;
-        if( x < n-1 &&  v[x+1][y] == 0 && isExist(board , word.substr(1) , v , x+1 , y)){
+        if( x < n-1 &&  v[x+1][y] == 0 && isExist(board , word , cur+1 , v , x+1 , y)){
             return true;
         }
-        if( y < m-1 && v[x][y+1] == 0 && isExist(board , word.substr(1), v,  x , y+1) ){
+        if( y < m-1 && v[x][y+1] == 0 && isExist(board , word , cur+1 , v,  x , y+1) ){
             return true;
         }
-        if( x > 0 && v[x-1][y] == 0 && isExist(board , word.substr(1) , v ,  x-1 , y) ){
+        if( x > 0 && v[x-1][y] == 0 && isExist(board , word , cur+1 , v ,  x-1 , y) ){
             return true;
         }
-        if( y > 0 && v[x][y-1] == 0 && isExist(board , word.substr(1) , v , x, y-1) ){
+        if( y > 0 && v[x][y-1] == 0 && isExist(board , word , cur+1 , v , x, y-1) ){
             return true;
         }
         
@@ -42,15 +42,15 @@ public:
         
         for( int i = 0 ; i < n ; ++i ){
             for( int j = 0 ; j < m ; ++j ){
-                // cout << board[i][j] << " ";
+  
                 if( word.length() == 1 && board[i][j] == word[0] ){
                     return true;
                 }
-                if( board[i][j] == word[0] && isExist(board , word , visit , i , j ) ){
+                if( board[i][j] == word[0] && isExist(board , word , 0 , visit , i , j ) ){
                     return true;
                 }
             }
-            // cout << endl;
+     
         }
         
         return false;
