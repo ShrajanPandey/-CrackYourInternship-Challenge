2fb89717a9ca5cc4a80f7b829628c3e1109f8eb8
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-07-01 23:03:37 +0530

    Time: 75 ms (20.67%), Space: 13.7 MB (29.97%) - LeetHub

---
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int mod = 1e9+7;
+    int countWays( int steps , int &arrLen , int cur , vector<vector<int>> &memo ){
+        
+        if( steps == 0 ){
+            if( cur == 0 ){
+                return 1;
+            }
+            return 0;
+        }
+            
+        if( memo[cur][steps] != -1 ){
+            return memo[cur][steps]%mod;
+        }
+        
+        int a = 0 , b = 0 , c = 0;
+        if( cur+ 1 < arrLen ){
+            a = countWays( steps-1 , arrLen , cur+1 , memo )%mod;
+        }
+        if( cur-1 >= 0 ){
+            b = countWays( steps-1 , arrLen, cur-1 , memo )%mod;
+        }
+        c = countWays( steps-1 , arrLen , cur , memo )%mod;
+        
+        return memo[cur][steps] = ((a+b)%mod+c)%mod;
+            
+    }
+    
+    int numWays(int steps, int arrLen) {
+        
+        vector<vector<int>> memo(steps+1 , vector<int> ( steps+2 , -1));
+        return countWays( steps , arrLen , 0 , memo );
+        
+    }
+};
