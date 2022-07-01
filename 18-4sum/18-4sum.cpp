886a47ec665d39dc0c47c662c4c8c9f12db589e8
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-07-02 01:43:22 +0530

    Time: 7 ms (99.77%), Space: 13.2 MB (77.07%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -4,14 +4,21 @@ public:
         
         vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
+        int n = nums.size();
         
         for( int i = 0 ; i < (int)nums.size()-3 ; ++i ){
             
             if( i == 0 || ( nums[i] != nums[i-1] ) ){
                 
+                if((long long)nums[i]+nums[i+1]+nums[i+2]+nums[i+3]>target) break;
+                if((long long)nums[i]+nums[n-3]+nums[n-2]+nums[n-1]<target) continue;
+                
                 for( int j = i+1 ; j < (int)nums.size()-2 ; ++j ){
-
+                    
                     if( j == i+1 || (nums[j] != nums[j-1]) ){
+                         
+                        if((long long)nums[i]+nums[j]+nums[j+1]+nums[j+2]>target) break;
+                        if((long long)nums[i]+nums[j]+nums[n-2]+nums[n-1]<target) continue;
                 
                         long long reqSum = (long long)target - nums[i] - nums[j];
                         int lo = j+1 , hi = nums.size()-1;
