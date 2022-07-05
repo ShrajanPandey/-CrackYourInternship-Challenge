8bf4fdc267df8b301ccabb668cbf2cd0377dc52f
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-07-05 16:57:57 +0530

    Time: 10 ms (80.87%), Space: 14 MB (49.15%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
-    int n , m;
-    
-    void dfs( vector<vector<int>> &img ,int sr , int sc , int newColor , int col){
+    void dfs( vector<vector<int>> &image , int sr , int sc , int color ){
         
-        if( sr >= n || sc >= m || sr < 0 || sc < 0 ){
+        if( sr < 0 || sc < 0 || sr >= image.size() || sc >= image[0].size() ){
             return;
         }
         
-        if( img[sr][sc] != col ){
-            return;
+        int curColor = image[sr][sc];
+        image[sr][sc] = color;
+        
+        if( sr + 1 < image.size() && image[sr+1][sc] == curColor ){
+            dfs(image, sr+1 , sc , color);
         }
-        
-        img[sr][sc] = newColor;
-        
-        dfs(img , sr+1 , sc , newColor , col);
-        dfs(img , sr-1 , sc , newColor , col);
-        dfs(img, sr , sc+1 , newColor , col);
-        dfs(img, sr , sc-1 , newColor , col);
-        
+        if( sc+1 < image[0].size() && image[sr][sc+1] == curColor ){
+            dfs(image , sr , sc+1 , color);
+        }
+        if( sc-1 >= 0 && image[sr][sc-1] == curColor ){
+            dfs(image , sr , sc-1 , color);
+        }
+        if( sr-1 >= 0 && image[sr-1][sc] == curColor ){
+            dfs(image , sr-1 , sc , color);
+        }
         
     }
     
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         
-        if( newColor == image[sr][sc] ){
+        if( image[sr][sc] == color ){
             return image;
-        }      
+        }
         
-        n = image.size(), m = image[0].size();
-        dfs(image , sr , sc , newColor , image[sr][sc]);
-        
+        dfs(image , sr , sc , color);
         return image;
         
     }
