1d0f1de5b6280d4917dc17468162c829e778d01c
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-07-05 15:13:12 +0530

    Time: 296 ms (16.46%), Space: 60 MB (12.57%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int dfs( unordered_map<int,vector<int>> &adjList , vector<int> &visited , int cur , int &no ){
+    void dfs( unordered_map<int,vector<int>> &adjList , vector<int> &visited , int cur ){
     
         visited[cur]  = 1;
-        int edges = adjList[cur].size();
-        ++no;
         
         for( auto neighbour : adjList[cur] ){
             if( visited[neighbour] == 0 ){
-                edges += dfs( adjList , visited , neighbour , no );
+                dfs( adjList , visited , neighbour );
             }
         }
-        
-        return edges;
-        
+
     }
     
     int makeConnected(int n, vector<vector<int>>& connections) {
+        
+        if( connections.size() < n-1 ){
+            return -1;
+        }
         
         unordered_map<int,vector<int>> adjList;
         
@@ -27,19 +27,12 @@ public:
         
         vector<int> visited(n ,0);
         int components = 0;
-        int extraEdges = 0;
-        
+     
         for( int i = 0 ; i < n ; ++i ){
             if( visited[i] == 0 ){
-                int no = 0;
-                int edges = dfs( adjList , visited , i , no);
-                extraEdges += (edges/2-no+1);
+                dfs( adjList , visited , i );
                 ++components;
             }
-        }
-        
-        if( extraEdges < components-1 ){
-            return -1;
         }
         
         return components-1;
