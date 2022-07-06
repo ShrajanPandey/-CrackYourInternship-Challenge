6b5335576b785abc317005d0a24e1085d5d1c878
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-07-06 14:48:00 +0530

    Time: 145 ms (43.45%), Space: 17.6 MB (6.09%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -11,68 +11,68 @@ public:
     
     }
     
-    int maxCherry( vector<vector<int>> &grid , int x1 , int y1 , int x2 , int y2, vector<vector<vector<int>>> &memo){
+    int maxCherry( vector<vector<int>> &grid , int x , int y1 , int y2, vector<vector<vector<int>>> &memo){
          
-        if( memo[x1][y1][y2] != -1 ){
-            return memo[x1][y1][y2];
+        if( memo[x][y1][y2] != -1 ){
+            return memo[x][y1][y2];
         }
         
         int cur = 0;
         
-        if( x1 == x2 && y1 == y2 ){
-            cur = grid[x1][y1];
+        if( y1 == y2 ){
+            cur = grid[x][y1];
         }
         else{
-            cur = grid[x1][y1] + grid[x2][y2];
+            cur = grid[x][y1] + grid[x][y2];
         }
         
-        if( x1 == n-1 && x2 == n-1 ){
-            return memo[x1][y1][y2] = cur;   
+        if( x == n-1 ){
+            return memo[x][y1][y2] = cur;   
         }
 
         
         int smallAns = 0;
-        if( isValid(x1+1,y1-1) ){
+        if( isValid(x+1,y1-1) ){
             
-            if( isValid(x2+1,y2-1) ){
-                smallAns = max( smallAns , maxCherry(grid , x1+1 , y1-1 , x2+1 , y2-1, memo));
+            if( isValid(x+1,y2-1) ){
+                smallAns = max( smallAns , maxCherry(grid , x+1 , y1-1 , y2-1, memo));
             }
-            if( isValid(x2+1,y2) ){
-                smallAns = max( smallAns , maxCherry(grid , x1+1 , y1-1 , x2+1 , y2 , memo));
+            if( isValid(x+1,y2) ){
+                smallAns = max( smallAns , maxCherry(grid , x+1 , y1-1 ,  y2 , memo));
             }
-            if( isValid(x2+1,y2+1) ){
-                smallAns = max( smallAns , maxCherry(grid , x1+1 , y1-1 , x2+1 , y2+1 , memo));
-            }
-            
-        }
-        if( isValid(x1+1,y1) ){
-            
-            if( isValid(x2+1,y2-1) ){
-                smallAns = max( smallAns , maxCherry(grid , x1+1 , y1 , x2+1 , y2-1 , memo));
-            }
-            if( isValid(x2+1,y2) ){
-                smallAns = max( smallAns , maxCherry(grid , x1+1 , y1 , x2+1 , y2 , memo));
-            }
-            if( isValid(x2+1,y2+1) ){
-                smallAns = max( smallAns , maxCherry(grid , x1+1 , y1 , x2+1 , y2+1 , memo));
+            if( isValid(x+1,y2+1) ){
+                smallAns = max( smallAns , maxCherry(grid , x+1 , y1-1 , y2+1 , memo));
             }
             
         }
-        if( isValid(x1+1,y1+1) ){
+        if( isValid(x+1,y1) ){
             
-            if( isValid(x2+1,y2-1) ){
-                smallAns = max( smallAns , maxCherry(grid , x1+1 , y1+1 , x2+1 , y2-1 , memo));
+            if( isValid(x+1,y2-1) ){
+                smallAns = max( smallAns , maxCherry(grid , x+1 , y1 , y2-1 , memo));
             }
-            if( isValid(x2+1,y2) ){
-                smallAns = max( smallAns , maxCherry(grid , x1+1 , y1+1 , x2+1 , y2 , memo));
+            if( isValid(x+1,y2) ){
+                smallAns = max( smallAns , maxCherry(grid , x+1 , y1 , y2 , memo));
             }
-            if( isValid(x2+1,y2+1) ){
-                smallAns = max( smallAns , maxCherry(grid , x1+1 , y1+1 , x2+1 , y2+1 , memo));
+            if( isValid(x+1,y2+1) ){
+                smallAns = max( smallAns , maxCherry(grid , x+1 , y1 , y2+1 , memo));
+            }
+            
+        }
+        if( isValid(x+1,y1+1) ){
+            
+            if( isValid(x+1,y2-1) ){
+                smallAns = max( smallAns , maxCherry(grid , x+1 , y1+1 , y2-1 , memo));
+            }
+            if( isValid(x+1,y2) ){
+                smallAns = max( smallAns , maxCherry(grid , x+1 , y1+1 , y2 , memo));
+            }
+            if( isValid(x+1,y2+1) ){
+                smallAns = max( smallAns , maxCherry(grid , x+1 , y1+1 , y2+1 , memo));
             }
             
         }
  
-        return memo[x1][y1][y2] = smallAns + cur;
+        return memo[x][y1][y2] = smallAns + cur;
         
     }
     
@@ -82,7 +82,7 @@ public:
         m = grid[0].size();
         vector<vector<vector<int>>> memo( n+1 , vector<vector<int>>( m+1 , vector<int>(m+1 , -1)));
         
-        return maxCherry(grid , 0 ,0, 0 , m-1 , memo);
+        return maxCherry(grid , 0 ,0 , m-1 , memo);
         
     }
 };
