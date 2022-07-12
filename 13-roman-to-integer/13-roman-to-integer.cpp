d247c154d3cedb743bc1bced2de2f4968422a373
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-07-12 20:55:54 +0530

    Time: 30 ms (22.99%), Space: 8.3 MB (21.09%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -2,40 +2,24 @@ class Solution {
 public:
     int romanToInt(string s) {
         
-        unordered_map<string,int> m;
-        m["I"] = 1;
-        m["V"] = 5;
-        m["X"] = 10;
-        m["L"] = 50;
-        m["C"] = 100;
-        m["D"] = 500;
-        m["M"] = 1000;
-        m["IV"] = 4;
-        m["IX"] = 9;
-        m["XL"] = 40;
-        m["XC"] = 90;
-        m["CD"] = 400;
-        m["CM"] = 900;
+        unordered_map<char,int> m;
+        m['I'] = 1;
+        m['V'] = 5;
+        m['X'] = 10;
+        m['L'] = 50;
+        m['C'] = 100;
+        m['D'] = 500;
+        m['M'] = 1000;
         
         int ans = 0;
-        for(int i = s.length()-1 ; i >= 0 ; --i ){
+        for(int i = 0 ; i < s.length() ; ++i ){
             
-            string p;
-            p += s[i];
-            
-            if( i > 0 ){
-                
-                string q = p;
-                q = s[i-1] + q;
-                if( m.count(q) ){
-                    ans += m[q];
-                    --i;
-                    continue;
-                }
-                
+            if( m[s[i+1]] > m[s[i]] ){
+                ans -= m[s[i]];
             }
-            
-            ans += m[p];
+            else{
+                ans += m[s[i]];
+            }
             
         }
         
