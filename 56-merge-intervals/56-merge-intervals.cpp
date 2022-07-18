0c4bc9980680396ce9e1ad956ba08e2f53d106e5
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-07-18 16:04:02 +0530

    Time: 44 ms (83.61%), Space: 18.9 MB (84.78%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -2,25 +2,32 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         
-        sort(intervals.begin(),intervals.end());
-        vector<vector<int>> ans;
-        int start = intervals[0][0] , end = intervals[0][1];
-        
-        for( int i = 0 ; i < intervals.size() ; ++i ){
-            if( intervals[i][0] > end ){
-                vector<int> a = {start,end};
-                ans.push_back(a);
-                end = intervals[i][1];
-                start = intervals[i][0];
-            }
-            else{
-                end = max(end,intervals[i][1]);
-            }
+        if( intervals.size() == 0 ){
+            return {{}};
         }
         
-        vector<int> a = {start,end};
-        ans.push_back(a);
+        vector<vector<int>> ans;
+    
+        sort( intervals.begin() , intervals.end() );
+        int lo = intervals[0][0] , hi = intervals[0][1];
         
+        int i = 1, n = intervals.size();
+        while( i < n ){
+            
+            if( hi < intervals[i][0] ){
+                ans.push_back({lo,hi});
+                lo = intervals[i][0];
+                hi = intervals[i][1];
+            }
+            else{
+                hi = max( hi , intervals[i][1]);
+                lo = min( lo , intervals[i][0]);
+            }
+            ++i;
+            
+        }
+        
+        ans.push_back({lo,hi});
         return ans;
         
     }
