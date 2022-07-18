249fe441190f38fab172cd37fbfa5f87301fe474
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-07-18 17:12:24 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (71.80%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -2,29 +2,35 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         
-        int i = m-1 , j = n-1 , k = n+m-1;
+        vector<int> ans;
+        int i = 0 , j = 0;
         
-        while( i >= 0 && j >= 0 ){
+        while( i < m || j < n ){
             
-            if( nums1[i] >= nums2[j] ){
-                nums1[k] = nums1[i];
-                --i;
-                --k;
+            if( i < m && j < n ){
+                if( nums1[i] < nums2[j] ){
+                    ans.push_back(nums1[i]);
+                    ++i;
+                }
+                else{
+                    ans.push_back(nums2[j]);
+                    ++j;
+                }
             }
-            else{
-                nums1[k] = nums2[j];
-                --j;
-                --k;
+            else if( i < m ){
+                ans.push_back(nums1[i]);
+                ++i;
             }
-            
-        } 
-        
-        while( j >= 0 ){
-            nums1[k] = nums2[j];
-            --j;
-            --k;
+            else if( j < n ){
+                ans.push_back(nums2[j]);
+                ++j;
+            }
         }
         
+        
+        for(int i = 0 ; i < ans.size() ; ++i ){
+            nums1[i] = ans[i];
+        }
         
     }
 };
