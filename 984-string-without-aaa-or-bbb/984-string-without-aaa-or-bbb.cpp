fba91ba6fcba055350bd5a4b4f1fb8c4229a9162
Shrajan Pandey <79400824+ShrajanPandey@users.noreply.github.com>
2022-07-24 18:58:17 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (30.22%) - LeetHub

--- 984-string-without-aaa-or-bbb/984-string-without-aaa-or-bbb.cpp
+++ 984-string-without-aaa-or-bbb/984-string-without-aaa-or-bbb.cpp
@@ -3,15 +3,24 @@ public:
     string strWithout3a3b(int a, int b, char aa = 'a' , char bb = 'b') {
         
         if( a < b ){
-            return strWithout3a3b( b , a , bb , aa );
+           return strWithout3a3b( b , a , bb , aa );
+        }
+
+        string ans;
+        
+        while( a-- > 0 ){
+            ans += aa;
+            if( a > b ){
+                ans += aa;
+                --a;
+            }
+            if( b ){
+                ans += bb;
+                --b;
+            }
         }
         
-        int a_no = min( 2 , a ) , b_no = (a-a_no >= b ? 1 : 0);
-        if( b == 0 ){
-            return string(a_no , aa);
-        }
-        
-        return string(a_no , aa) + string(b_no , bb ) + strWithout3a3b( a-a_no , b-b_no, aa , bb);
+        return ans;
         
     }
 };
